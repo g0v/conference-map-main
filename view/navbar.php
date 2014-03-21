b6a749645bb4841fd24692bcdb2f12a16b4bd0ca
Youchen Lee <copyleft@utcr.org>
2014-03-21 20:36:57 +0800

    add about

--- view/navbar.php
+++ view/navbar.php
@@ -18,7 +18,8 @@ if (strlen($PAGE['ACTIVITY_INFO']['ACTIVITY_ID']) > 0) {
         <h1><a class="logo" style="border-style:none;" href="/"></a><p>for <?php echo $PAGE['ACTIVITY_INFO']['ACTIVITY_ID'];?></p></h1>
         <span id="activity_id"><?php echo $PAGE['ACTIVITY_INFO']['ACTIVITY_ID'];?></span>
         <ul id="nav">
-            <li class="<?php echo ($cur_tab == 'chat-map') ? 'cur': '';?> mobile_float_r"><a href="<?php echo $base_url;?>">聊天地圖</a></li>
+            <li class="<?php echo ($cur_tab == 'chat-map') ? 'cur': '';?> mobile_float_r"><a href="/">聊天地圖</a></li>
+            <li class="<?php echo ($cur_tab == 'about') ? 'cur': '';?> mobile_float_r"><a href="/about">About</a></li>
         </ul>
     </div>
 </div><!--#hd_wrap-->
--- view/page/about.php
+++ view/page/about.php
@@ -26,42 +26,20 @@ EOF;
     );
     Page::load('navbar', $view_data);
     ?>
+<a href="https://github.com/UniSharp/conference-map-main/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://github-camo.global.ssl.fastly.net/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
     <div id="bd"  class="grid_12 clearfix">
     <div id="bd_wrap" class="clearfix">
         <div id="myprogram_timeline">
             <div id="about_wrap" class="feed">
-            <h3>關於「我的活動地圖」：</h3>
+            <h3>關於</h3>
+            <p>請注意，本網站無 SSL 加密，且您發表的訊息每個人都可以看到，因此請勿傳送任何隱私資訊。</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <hr />
-            <p>
-            <ol>
-                <li>感謝您關注我們的產品 - <a href="/" class="custom_btn special_btn" target="_blank">我的活動地圖 for COSCUP 2013</a>。</li>
-                <li>有感於 <a href="http://coscup.org/2013/zh-tw/" class="custom_btn" target="_blank">COSCUP 2013</a> 的八軌議程實在讓人太難抉擇，促成了這個小工具，希望您會喜歡。如果有任何覺得需要改進的地方，歡迎您 <a href="https://docs.google.com/forms/d/1LkO0cvJXG9882LtNIdRKLDtFOZpbHqrjtABr9117mUM/viewform?edit_requested=true" target="_blank" class="custom_btn special_btn">提供意見</a>給我們，或是加入我們的 <a href="https://www.facebook.com/confmap" class="custom_btn special_btn" target="_blank">Facebook 粉絲團</a>，持續關注我們。</li>
-                <li>我們是 <a class="custom_btn" href="http://coscup.org/2013/zh-tw/" target="_blank">COSCUP 2013</a> 的贊助商之一，但不是籌備單位。以下的聲明，均是我們的私人立場，與 COSCUP 主辦單位無關。</li>
-            </ol>
-            </p>
-
-            <h3>給正常人：</h3>
-            <hr />
-            <p>
-            <ol>
-                <li><a class="custom_btn" href="http://coscup.org/2013/zh-tw/about/" target="_blank">COSCUP</a> 是全台灣最大的自由軟體、開放原始碼軟體研討會。詳細的介紹請參考<a class="custom_btn" href="http://coscup.org/2013/zh-tw/about/" target="_blank">COSCUP 官方網站</a>。</li>
-                <li><a href="/" class="custom_btn special_btn" target="_blank">我的活動地圖</a> 是一個幫助研討會參與者選擇並記錄自己想參加的議程、規劃動線的小工具。</li>
-            </ol>
-            </p>
-
-            <h3>給工程師：</h3>
-            <hr />
-            <p>
-            <ol>
-                <li>我們鼓勵您多選擇朋友沒參加的議程，每一個 <a class="custom_btn" href="http://coscup.org" target="_blank">COSCUP</a> 演講內容，都是講者與其他眾多開發者多年奉獻的心血結晶，希望您與您的朋友們利用這個工具，分散彼此參與的議程，並於 <a class="custom_btn" href="http://coscup.org" target="_blank">COSCUP</a> 會後彼此分享，確保這些心血被廣泛的散播與吸收。我們雖提供每個人設定自己感興趣的議程，但為了避免講者與主辦單位困擾，本網站不做議程熱門程度統計。</li>
-                <li>本網站的議程資料來自 <a class="custom_btn" href="http://coscup.org/2013/zh-tw/api/" target="_blank">COSCUP API</a> 。為了不影響 <a class="custom_btn" href="http://coscup.org" target="_blank">COSCUP</a> 的網站流量，我們不會即時從 <a class="custom_btn" href="http://coscup.org" target="_blank">COSCUP</a> 網站更新議程資訊。如果您發現議程資訊錯誤，歡迎您告訴我們。</li>
-                <li>本網站會使用 Cookie 來記錄您的趕場路線圖 ID, 以及被您加入頁籤的朋友趕場路線圖 ID （目前只會保留最近的三個朋友頁籤）</li>
-
-                <?php #<!-- //this is buggy, use it only when you read this line <li> * 我們支援 Application Cache 技術，確保您能在網路連線狀況不佳的環境，也能觀看地圖以及議程資訊。因此建議您可以在有網路的地方，點選「使用 cache」瀏覽一次網站，並同意瀏覽器使用離線瀏覽功能，讓您的瀏覽器先 cache 住議程資訊。您也可以手動在網址後面加上 ?cache=1 來啟用 cache。</li>-->?>
-            </ol>
-            </p>
-            <hr />
-            <div><a href="http://www.unisharp.com">Powered by <img alt="UniSharp - 悠下爾科技股份有限公司" src="http://www.unisharp.com/images/unisharp.gif" /></a></div>
             </div>
         </div>
     </div><!--bd_wrap-->
