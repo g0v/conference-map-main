caf5d13d153ec14fe48193d67ebb8089a4b42f5b
Youchen Lee <copyleft@utcr.org>
2014-04-19 15:22:03 +0800

    apply gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,24 @@ var concat = require('gulp-concat');
 
 var paths = {
     liveScripts: './assets/ls/*.ls',
+    liveScriptsDest: './assets/js/',
 };
 
-// compile react scripts and concat into one
 gulp.task('liveScripts', function(){
-    // place code for your default task here
-    // gulp.src(paths.liveScripts)
-    //     .pipe(react())
-    //     .pipe(concat('expr.react.all.js'))
-    //     .pipe(gulp.dest('./static/js/v2/build'));
+    gulp.src(paths.liveScripts)
+         .pipe(liveScript({bare: true}))
+         .pipe(gulp.dest(paths.liveScriptsDest));
+});
+
+gulp.task('build', ['liveScripts'],  function(){
+});
+
+gulp.task('dev', ['build'],  function(){
+    var watcher = gulp.watch(paths.liveScripts, ['liveScripts']);
+    watcher.on('change', function(event) {
+        console.log('File '+event.path+' was '+event.type+', running tasks...');
+    });
+
 });
 
 // The default task (called when you run `gulp` from cli)
